1a1b8310cc23a206d19bc5f1ffcd845b3a5fa670
DAB <dab@dab.la>
2020-06-24 23:35:26 -0400

    Update config load and logger

--- Tools/Configuration.php
+++ Tools/Configuration.php
@@ -4,6 +4,7 @@ namespace lightningsdk\sitemanager\Tools;
 
 use lightningsdk\core\Tools\Cache\Cache;
 use lightningsdk\core\Tools\ConfigurationCore;
+use lightningsdk\core\Tools\Logger;
 use lightningsdk\sitemanager\Model\Site;
 
 class Configuration extends ConfigurationCore {
@@ -14,6 +15,7 @@ class Configuration extends ConfigurationCore {
             if ($cached_config = $cache->get(Site::getInstance()->domain . '_config')) {
                 // override the entire config
                 static::$configuration = $cached_config;
+                Logger::debugf('Loading from cache for domain %s found %s', Site::getInstance()->domain, json_encode($cached_config));
                 return true;
             }
         }
